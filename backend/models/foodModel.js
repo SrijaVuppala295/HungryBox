080b79860207083c1fbf15ad3b5bbcc0ef9ce68d
Palak Agarwal <112793075+palakag29@users.noreply.github.com>
2025-01-05 16:18:12 +0000

    Customize Ratings as per User Reviews

--- backend/models/foodModel.js
+++ backend/models/foodModel.js
@@ -1,5 +1,11 @@
 import mongoose from "mongoose";
 
+const reviewSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  rating: { type: Number, required: true, min: 1, max: 5 }, // Rating between 1 and 5
+  comment: { type: String }
+});
+
 const foodSchema = new mongoose.Schema({
   name: { type: String, required: true },
   description: { type: String, required: true },
@@ -8,6 +14,8 @@ const foodSchema = new mongoose.Schema({
   category: { type: String, required: true },
   day: { type: String, required: true }, // e.g., "Thursday"
   timeSlot: { type: String, required: true }, // e.g., "Lunch", "Dinner"
+  reviews: [reviewSchema], 
+  averageRating: { type: Number, default: 0 } 
 });
 
 const foodModel = mongoose.models.food || mongoose.model("food", foodSchema);
