0b45f40b803ff1ef8f7978113589314cfa6fea44
712Kunal <jadhavkun0@gmail.com>
2025-01-14 22:03:24 +0530

    update db.ts file which causing error in connecting database

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
-const URI = process.env.MONGO_URI as string
 export const connectDB = async () => {
+  const URI = process.env.MONGO_URI as string
   try {
     await mongoose.connect(URI);
     console.log("DB Connected");
