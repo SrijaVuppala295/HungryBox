cbcd34ab39045ffa8f55af4665df263640ce229c
Aadi Joshi <toaadijoshi@gmail.com>
2025-01-15 14:34:12 +0530

    Added a significant kitchen to doorstep block to the home page

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -4,14 +4,16 @@ import Header from "../../components/Header/Header";
 import ExploreMenu from "../../components/ExploreMenu/ExploreMenu";
 import FoodDisplay from "../../components/FoodDisplay/FoodDisplay";
 import Feedback from "../../components/FeedbackForm/Feedback";
+import KitchenToDoorstep from '../../components/KitchenToDoorstep/KitchenToDoorstep';
 
 const Home = () => {
   const [category, setCategory] = useState("All");
   return (
-    <div>
+    <div className="home">
       <Header />
       <ExploreMenu category={category} setCategory={setCategory} />
       <FoodDisplay category={category} />
+      <KitchenToDoorstep />
       <Feedback />
     </div>
   );
