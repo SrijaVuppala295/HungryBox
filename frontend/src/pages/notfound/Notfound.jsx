ea3a5c93099b4657c78a2de03f47c654fb468ddc
712Kunal <jadhavkun0@gmail.com>
2025-02-15 23:33:44 +0530

    Done with the 404 not found page

--- frontend/src/pages/notfound/Notfound.jsx
+++ frontend/src/pages/notfound/Notfound.jsx
@@ -5,13 +5,17 @@ function Notfound() {
   return (
     <div className="notfound-page">
       {/* Spline Background */}
-      <div className="spline-component">
-        
+      <div className="video-component">
+        <video autoPlay loop muted src="/404.mp4"></video>
       </div>
 
       {/* Content */}
       <div className="content">
-        Notfound
+        <h1>Oops!</h1>
+        <p>Page not found</p>
+        <button onClick={() => (window.location.href = "/")}>
+          <a href="/">Go back to home</a>
+        </button>
       </div>
     </div>
   );
